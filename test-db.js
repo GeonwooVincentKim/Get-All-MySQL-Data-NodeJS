7b7720590f36d1894068ada2e8012973c77ae3f8
GeonwooVincentKim <kdsnop@gmail.com>
2021-01-19 16:33:04 +0900

    After create `test.js` file

---
+++ test-db.js
@@ -0,0 +1,33 @@
+var mysql = require("mysql");
+var con = mysql.createConnection({
+    host: "localhost",
+    port: "3306",
+    user: "root",
+    password: "1234",
+    database: "GameDB"
+});
+
+con.connect(function(err){
+    if(err) throw err;
+    console.log("connected");
+    con.query("SELECT * FROM TB_USER", function(err, result, fields){
+        if(err) throw err;
+        console.log(result);
+    });
+    con.query("SELECT * FROM TB_IMAGE", function(err, result, fields){
+        if(err) throw err;
+        console.log(fields);
+        // result.render('TB_IMAGE', {ID: })
+    });
+    con.query("SELECT * FROM TB_GAME", function(err, result, fields){
+        if(err) throw err;
+        console.log(fields);
+    });
+    // var resultArray = err.values(JSON.parse(JSON.stringify(err)));
+    // print(resultArray);
+});
+module.exports = con;
+// con.query(function(err){
+//     if(err) throw err;
+//     console.log("Parsing Data..");
+// });
